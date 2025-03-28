a9eaddff06d292b475a7c71609df33d7fd8727ac
Pranavdev007 <pranavdev1999@gmail.com>
2025-03-28 15:05:26 +0530

    Update 14_timeoutCancellation.js

--- 14_timeoutCancellation.js
+++ 14_timeoutCancellation.js
@@ -18,24 +18,6 @@ const cancelTimeMs = 50;
 const cancelFn = cancellable((x) => x * 5, [2], 20);
 setTimeout(cancelFn, cancelTimeMs);
 The cancellation was scheduled to occur after a delay of cancelTimeMs (50ms), which happened after the execution of fn(2) at 20ms.
-
-Example 2:
-Input: fn = (x) => x**2, args = [2], t = 100
-Output: []
-Explanation: 
-const cancelTimeMs = 50;
-const cancelFn = cancellable((x) => x**2, [2], 100);
-setTimeout(cancelFn, cancelTimeMs);
-The cancellation was scheduled to occur after a delay of cancelTimeMs (50ms), which happened before the execution of fn(2) at 100ms, resulting in fn(2) never being called.
-
-Example 3:
-Input: fn = (x1, x2) => x1 * x2, args = [2,4], t = 30
-Output: [{"time": 30, "returned": 8}]
-Explanation: 
-const cancelTimeMs = 100;
-const cancelFn = cancellable((x1, x2) => x1 * x2, [2,4], 30);
-setTimeout(cancelFn, cancelTimeMs);
-The cancellation was scheduled to occur after a delay of cancelTimeMs (100ms), which happened after the execution of fn(2,4) at 30ms.
  
 Constraints:
 fn is a function
