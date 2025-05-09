f4ed767b0cba0f62e52fdd5f6c193d5493605d09
Pranavdev007 <pranavdev1999@gmail.com>
2025-05-10 01:21:36 +0530

    Update 20_isObjectEmpty.js

--- 20_isObjectEmpty.js
+++ 20_isObjectEmpty.js
@@ -1,6 +1,4 @@
 // Question Link: https://leetcode.com/problems/is-object-empty/description/?envType=study-plan-v2&envId=30-days-of-javascript
-// Solution Link: https://leetcode.com/problems/is-object-empty/solutions/5446306/2-easy-one-line-javascript-solution-using-loops-and-predefined-function/
-
 /*
 2727. Is Object Empty
 
@@ -14,24 +12,6 @@ Input: obj = {"x": 5, "y": 42}
 Output: false
 Explanation: The object has 2 key-value pairs so it is not empty.
 
-Example 2:
-Input: obj = {}
-Output: true
-Explanation: The object doesn't have any key-value pairs so it is empty.
-
-Example 3:
-Input: obj = [null, false, 0]
-Output: false
-Explanation: The array has 3 elements so it is not empty.
- 
-Constraints:
-obj is a valid JSON object or array
-2 <= JSON.stringify(obj).length <= 10^5 
-
-Can you solve it in O(1) time?
-*/
-
-
 
 /**
  * @param {Object|Array} obj
@@ -39,12 +19,12 @@ Can you solve it in O(1) time?
  */
 
 // 1st Approach
-var isEmpty = function(obj) {
-    for (const _ in obj) {
-        return false;
-    }
+var isEmpty = function (obj) {
+  for (const _ in obj) {
+    return false;
+  }
 
-    return true;
+  return true;
 };
 
 /*
