7c5ab848fa01ff703e3be5deac26fce59b5e5ea8
Pranavdev007 <pranavdev1999@gmail.com>
2025-05-13 22:10:14 +0530

    Update 22_arrayPrototypeLast.js

--- 22_arrayPrototypeLast.js
+++ 22_arrayPrototypeLast.js
@@ -1,5 +1,4 @@
 // Question Link: https://leetcode.com/problems/array-prototype-last/description/?envType=study-plan-v2&envId=30-days-of-javascript
-// Solution Link: https://leetcode.com/problems/array-prototype-last/solutions/5446355/javascript-easy-solution-one-line-code/
 
 /*
 2619. Array Prototype Last
@@ -11,24 +10,12 @@ Example 1:
 Input: nums = [null, {}, 3]
 Output: 3
 Explanation: Calling nums.last() should return the last element: 3.
-Example 2:
-Input: nums = []
-Output: -1
-Explanation: Because there are no elements, return -1.
-
-Constraints:
-arr is a valid JSON array
-0 <= arr.length <= 1000
-*/
-
-
 
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
-Array.prototype.last = function() {
-    
-    return this.length ? this[this.length - 1] : -1;
+Array.prototype.last = function () {
+  return this.length ? this[this.length - 1] : -1;
 };
 
 /**
