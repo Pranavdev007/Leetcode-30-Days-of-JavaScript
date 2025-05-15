33051bf08a475bac327ce0b6fb0b497b366b08e7
Pranavdev007 <pranavdev1999@gmail.com>
2025-05-16 00:19:43 +0530

    Update 23_groupBy.js

--- 23_groupBy.js
+++ 23_groupBy.js
@@ -1,5 +1,4 @@
 // Question Link: https://leetcode.com/problems/group-by/description/?envType=study-plan-v2&envId=30-days-of-javascript
-// Solution Link: https://leetcode.com/problems/group-by/solutions/5446407/2-easy-javascript-solution/
 
 /*
 2631. Group By
@@ -32,47 +31,11 @@ The selector function gets the "id" out of each item in the array.
 There are two objects with an "id" of 1. Both of those objects are put in the first array.
 There is one object with an "id" of 2. That object is put in the second array.
 
-Example 2:
-Input: 
-array = [
-  [1, 2, 3],
-  [1, 3, 5],
-  [1, 5, 9]
-]
-fn = function (list) { 
-  return String(list[0]); 
-}
-Output: 
-{ 
-  "1": [[1, 2, 3], [1, 3, 5], [1, 5, 9]] 
-}
-Explanation:
-The array can be of any type. In this case, the selector function defines the key as being the first element in the array. 
-All the arrays have 1 as their first element so they are grouped together.
-{
-  "1": [[1, 2, 3], [1, 3, 5], [1, 5, 9]]
-}
-
-Example 3:
-Input: 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-fn = function (n) { 
-  return String(n > 5);
-}
-Output:
-{
-  "true": [6, 7, 8, 9, 10],
-  "false": [1, 2, 3, 4, 5]
-}
-Explanation:
-The selector function splits the array by whether each number is greater than 5.
  
 Constraints:
 0 <= array.length <= 10^5
 fn returns a string
 */
-
-
 
 /**
  * @param {Function} fn
@@ -80,23 +43,21 @@ fn returns a string
  */
 
 // 1st Approach
-Array.prototype.groupBy = function(fn) {
+Array.prototype.groupBy = function (fn) {
+  const result = {};
 
-    const result = {};
+  for (let i = 0; i < this.length; i++) {
+    const key = fn(this[i]);
 
-    for (let i = 0; i < this.length; i++) {
-        const key = fn(this[i]);
-
-        if (result[key]) {
-            result[key].push(this[i]);
-        } else {
-            result[key] = [this[i]];
-        }
+    if (result[key]) {
+      result[key].push(this[i]);
+    } else {
+      result[key] = [this[i]];
     }
+  }
 
-    return result;
+  return result;
 };
-
 
 /*
 // 2nd Approach
