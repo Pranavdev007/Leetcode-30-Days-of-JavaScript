fde941b18c843e4ba2f94b93c4dfffec4281c622
Pranavdev007 <pranavdev1999@gmail.com>
2025-06-01 22:20:53 +0530

    Update 27_compactObject.js

--- 27_compactObject.js
+++ 27_compactObject.js
@@ -1,5 +1,4 @@
 // Question Link: https://leetcode.com/problems/compact-object/description/?envType=study-plan-v2&envId=30-days-of-javascript
-// Solution Link: https://leetcode.com/problems/compact-object/solutions/5458268/easy-javascript-solution/
 
 /*
 2705. Compact Object
@@ -12,16 +11,6 @@ Example 1:
 Input: obj = [null, 0, false, 1]
 Output: [1]
 Explanation: All falsy values have been removed from the array.
-
-Example 2:
-Input: obj = {"a": null, "b": [false, 1]}
-Output: {"b": [1]}
-Explanation: obj["a"] and obj["b"][0] had falsy values and were removed.
-
-Example 3:
-Input: obj = [null, 0, 5, [0], [false, 16]]
-Output: [5, [], [16]]
-Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
  
 Constraints:
 obj is a valid JSON object
